4a8c6b84ec5abcf5c1a3375289687afbe006fc7c
qlhan <qlhan@incarcloud.com>
2024-06-05 11:35:56 +0800

    test: LCD With TFT_eSPI
    esp32s3

--- README.md
+++ README.md
@@ -21,3 +21,25 @@ trusted-host = mirrors.aliyun.com
 ```bash
 ~\.platformio\penv\Scripts\pio run -t build
 ```
+
+## 3 0.96inch_LCD
+
+### 3.1 User_Setup.h
+
+```c
+#define ST7735_DRIVER
+
+#define TFT_WIDTH  80
+#define TFT_HEIGHT 160
+
+#define TFT_MOSI 5
+#define TFT_SCLK 4
+#define TFT_CS   15
+#define TFT_DC   7
+#define TFT_RST  6
+#define TFT_BL   16
+```
+
+### 3.2 User_Setup_Select.h
+
+`NA`
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,39 @@
 #include <Arduino.h>
+
+///*
+/////////////// ch02 LCD With TFT_eSPI ///////////////
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+// lib_deps = bodmer/TFT_eSPI@^2.5.43
+void setup() {
+    Serial.begin(9600);
+
+    // 初始化屏幕
+    tft.begin();
+    // 设置屏幕旋转方向，这里设置为1，根据需要可调整
+    tft.setRotation(1);
+    // 填充屏幕为黑色
+    tft.fillScreen(TFT_BLACK);
+    // 设置文本颜色为白色，背景为黑色
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    // 设置字体大小
+    tft.setTextSize(2);
+    // 设置文本起始位置，x坐标和y坐标
+    tft.setCursor(40, 60);
+    // 打印"Hello World"
+    tft.println("Hello World");
+}
+
+void loop() {
+    Serial.println("lcd display");
+    delay(1000);
+}
+//*/
+
+/*
+/////////////// ch01 blink ///////////////
 #include "Freenove_WS2812_Lib_for_ESP32.h"
 
 #define LEDS_COUNT 1
@@ -8,6 +43,7 @@
 Freenove_ESP32_WS2812 strip =
         Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL, TYPE_GRB);
 
+// lib_deps = freenove/Freenove WS2812 Lib for ESP32@^1.0.6
 void setup() {
     Serial.begin(9600);
 
@@ -27,3 +63,4 @@ void loop() {
         delay(10);
     }
 }
+*/
